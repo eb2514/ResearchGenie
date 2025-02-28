7999714dce49de73d32e5cf4569bc86726837b63
Elay Binyamin <72104388+eb2514@users.noreply.github.com>
2025-02-27 23:28:05 -0500

    Update rag_v4_cuda.py

--- rag_v4_cuda.py
+++ rag_v4_cuda.py
@@ -87,7 +87,7 @@ def query_chroma(query):
     # Initialize the Chroma store
     embedding_model = HuggingFaceEmbeddings(
     model_name="sentence-transformers/all-MiniLM-L6-v2", 
-    model_kwargs={"device":"cpu"}
+    model_kwargs={"device":"cuda"}
     )
     chroma_store = Chroma(persist_directory="chroma_db_v4", embedding_function=embedding_model, collection_name="my_collection")
 
