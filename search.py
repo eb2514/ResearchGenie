346940844b06b54cdec65bdb3b862213456d68d3
Elay Binyamin <72104388+eb2514@users.noreply.github.com>
2025-03-03 14:20:26 -0500

    Update search.py

--- search.py
+++ search.py
@@ -31,27 +31,10 @@ st.markdown(
     """,
     unsafe_allow_html=True
 )
-
 def send_query(prompt):
-    with st.spinner("Searching..."):
+    with st.spinner("Crafting Response..."):
         query = query_chroma(prompt)
-        #response = f"""**{query.page_content}**  \n """
-        response =""
-        for document in query:
-            response += create_response(document)
-        return response             
-
-def create_response(document):
-    #file_path = document.metadata['source']
-    llm = ChatOllama(model="llama3.2:3b")
-    #result = llm.invoke(f"Summarize this in 100 words or less in bullet points with a newline after each, do not include introduction: {document.page_content}")
-    result = llm.invoke(f"Summarize this in 100 words or less in bullet points with a newline after each, do not include introduction: Chocolate chip cookie recipe")
-    #with open(file_path, "rb") as file:
-        #file_bytes = file.read() 
-        #pdf_base64 = base64.b64encode(file_bytes).decode("utf-8")  
-        #response = f"""  \n *{result.content}*  \n <iframe src="data:application/pdf;base64,{pdf_base64}#page={document.metadata["page"]}" width="80%" height="1000px"></iframe>  \n """
-    response = f"""  \n *{result.content}*  \n " width="80%" height="1000px"></iframe>  \n """
-    return response
+    return query
     
 if "chats" not in st.session_state:
     st.session_state.chats = {"Default Chat": []}
