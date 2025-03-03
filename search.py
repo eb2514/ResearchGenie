8fe006468649d6ed724c6bc1cafb1b834b3fb7a6
Elay Binyamin <72104388+eb2514@users.noreply.github.com>
2025-03-02 22:15:34 -0500

    Update search.py

--- search.py
+++ search.py
@@ -37,8 +37,8 @@ def send_query(prompt):
         query = query_chroma(prompt)
         #response = f"""**{query.page_content}**  \n """
         response =""
-        #for document in query:
-        #    response += create_response(document)
+        for document in query:
+            response += create_response(document)
         return query             
 
 def create_response(document):
