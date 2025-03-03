19981b5dac0170c21dcf832e40e564de15f526da
Elay Binyamin <72104388+eb2514@users.noreply.github.com>
2025-03-03 14:33:47 -0500

    Update rag_v4_cuda.py

--- rag_v4_cuda.py
+++ rag_v4_cuda.py
@@ -21,23 +21,36 @@ def remove_duplicate_results(results):
             seen.add(content)
             unique_results.append(result)
     return unique_results
+
+def create_response(unique_results):
+    response = ""
+    for document in unique_results:    
+        file_path = document.metadata['source']
+        llm = ChatOllama(model="llama3.2:3b")
+        result = llm.invoke(f"Summarize this in 100 words or less in bullet points with a newline after each, do not include introduction: {document.page_content}")
+        with open(file_path, "rb") as file:
+            file_bytes = file.read() 
+            pdf_base64 = base64.b64encode(file_bytes).decode("utf-8")  
+            response += f"""  \n *{result.content}*  \n <iframe src="data:application/pdf;base64,{pdf_base64}#page={document.metadata["page"]}" width="80%" height="1000px"></iframe>  \n """
+    return response
     
 def query_chroma(query):
     # Initialize the Chroma store
     embedding_model = HuggingFaceEmbeddings(
     model_name="sentence-transformers/all-MiniLM-L6-v2")
     print("Communicating with server")
-    #client = chromadb.HttpClient(host=st.secrets['IP_ADDRESS'], port=8000)
+    client = chromadb.HttpClient(host=st.secrets['IP_ADDRESS'], port=8000)
     print("Communicating with server")
-    #chroma_store = Chroma(client=client, embedding_function=embedding_model, collection_name="my_collection")
+    chroma_store = Chroma(client=client, embedding_function=embedding_model, collection_name="my_collection")
     print("Communicating with server")
 
     # Query the database
     print(f"Querying for: '{query}'")
-    #results = chroma_store.similarity_search(query, k=3)
-    #unique_results = remove_duplicate_results(results)
-    unique_results = ["A","B"]
-    return unique_results
+    results = chroma_store.similarity_search(query, k=3)
+    unique_results = remove_duplicate_results(results)
+    response = create_response(unique_results)
+    #unique_results = ["A","B"]
+    return response
     # for i, result in enumerate(unique_results, start=1):
     #     #print(results)
     #     print(f"\nResult {i}:")
