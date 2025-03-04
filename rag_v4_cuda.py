bd0d534a0eb471be6579478a416c635b27eae3fd
Elay Binyamin <72104388+eb2514@users.noreply.github.com>
2025-03-04 17:36:54 -0500

    Update rag_v4_cuda.py

--- rag_v4_cuda.py
+++ rag_v4_cuda.py
@@ -1,5 +1,3 @@
-
-
 __import__('pysqlite3')
 import sys
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
@@ -29,13 +27,12 @@ def create_response(unique_results):
     response = ""
     for document in unique_results:    
         file_path = document.metadata['source']
-        #llm = ChatOllama(model="llama3.2:3b")
-        #result = llm.invoke(f"Summarize this in 100 words or less in bullet points with a newline after each, do not include introduction: {document.page_content}")
+        llm = ChatOllama(model="llama3.2:3b")
+        result = llm.invoke(f"Summarize this in 100 words or less in bullet points with a newline after each, do not include introduction: {document.page_content}")
         with open(file_path, "rb") as file:
             file_bytes = file.read() 
             pdf_base64 = base64.b64encode(file_bytes).decode("utf-8")  
-            #response += f"""  \n *{result.content}*  \n <iframe src="data:application/pdf;base64,{pdf_base64}#page={document.metadata["page"]}" width="80%" height="1000px"></iframe>  \n """
-            response += f"""  \n <iframe src="data:application/pdf;base64,{pdf_base64}#page={document.metadata["page"]}" width="80%" height="1000px"></iframe>  \n """
+            response += f"""  \n *{result.content}*  \n <iframe src="data:application/pdf;base64,{pdf_base64}#page={document.metadata["page"]}" width="80%" height="1000px"></iframe>  \n """
     return response
     
 def query_chroma(query):
@@ -52,26 +49,5 @@ def query_chroma(query):
     print(f"Querying for: '{query}'")
     results = chroma_store.similarity_search(query, k=3)
     unique_results = remove_duplicate_results(results)
-    response = create_response(unique_results)
-    #unique_results = ["A","B"]
-    return response
-    # for i, result in enumerate(unique_results, start=1):
-    #     #print(results)
-    #     print(f"\nResult {i}:")
-    #     print("\n----------------------------\n")
-    #     print(f"Page Content: {result.page_content}")
-    #     print("\n----------------------------\n")
-    #     print(f"Metadata: {result.metadata}")
-    #     print("\n----------------------------\n")
-    #     print(f"{result.metadata['source']}, {result.metadata['page']}")
-
-# if __name__ == '__main__':
-#     start = time.time()
-#     #ingest(pdf_directory="C://Users/elayb/OneDrive/Desktop/Pubmed", chroma_db_path="chroma_db_v4")  #"C://Users/elayb/OneDrive/Desktop/Pubmed"
-#     query_chroma(query="What are the side effects of Caffiene on Vision")
-#     end = time.time()
-    
-#     print(end-start)
-
-# Example query
-#
+    #response = create_response(unique_results)
+    return unique_results
