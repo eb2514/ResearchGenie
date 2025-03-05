96da6704ca80c51bd14e1f676189523346f8f71c
Elay Binyamin <72104388+eb2514@users.noreply.github.com>
2025-03-04 22:03:43 -0500

    Update rag_v4_cuda.py

--- rag_v4_cuda.py
+++ rag_v4_cuda.py
@@ -42,7 +42,7 @@ def create_response(unique_results):
     for document in unique_results:
         file_path = document.metadata['source']
         file_name = os.path.basename(file_path).replace('\\', '/')
-        file_url = f'{file_endpoint}{file_path}'
+        file_url = f'{file_endpoint}{file_name}'
         st.write(file_url)
         response = requests.get(file_url)
         if response.status_code == 200:
