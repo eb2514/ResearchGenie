4990d527078e60187fc3df084f046108012a849d
Elay Binyamin <72104388+eb2514@users.noreply.github.com>
2025-03-04 20:37:11 -0500

    Update rag_v4_cuda.py

--- rag_v4_cuda.py
+++ rag_v4_cuda.py
@@ -4,10 +4,11 @@ sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings.huggingface import HuggingFaceEmbeddings
 from langchain_community.chat_models import ChatOllama
+import base64
+import requests
 import os
 import time
 import chromadb
-import base64
 import streamlit as st
 
 
@@ -23,16 +24,29 @@ def remove_duplicate_results(results):
             unique_results.append(result)
     return unique_results
 
+# def create_response(unique_results):
+#     response = ""
+#     for document in unique_results:    
+#         file_path = document.metadata['source']
+#         llm = ChatOllama(model="llama3.2:3b")
+#         result = llm.invoke(f"Summarize this in 100 words or less in bullet points with a newline after each, do not include introduction: {document.page_content}")
+#         with open(file_path, "rb") as file:
+#             file_bytes = file.read() 
+#             pdf_base64 = base64.b64encode(file_bytes).decode("utf-8")  
+#             response += f"""  \n *{result.content}*  \n <iframe src="data:application/pdf;base64,{pdf_base64}#page={document.metadata["page"]}" width="80%" height="1000px"></iframe>  \n """
+#     return response
+
 def create_response(unique_results):
     response = ""
-    for document in unique_results:    
+    file_endpoint = st.secrets['file_endpoint']
+    for document in unique_results:
         file_path = document.metadata['source']
-        llm = ChatOllama(model="llama3.2:3b")
-        result = llm.invoke(f"Summarize this in 100 words or less in bullet points with a newline after each, do not include introduction: {document.page_content}")
-        with open(file_path, "rb") as file:
-            file_bytes = file.read() 
-            pdf_base64 = base64.b64encode(file_bytes).decode("utf-8")  
-            response += f"""  \n *{result.content}*  \n <iframe src="data:application/pdf;base64,{pdf_base64}#page={document.metadata["page"]}" width="80%" height="1000px"></iframe>  \n """
+        pdf_base64 = base64.b64encode(file_bytes).decode("utf-8")
+        file_url = f'{file_endpoint}{file_path}'
+        response = requests.get(file_url)
+        if response.status_code == 200:
+            pdf_base64 = base64.b64encode(response.content).decode("utf-8")
+            response += f"""  \n <iframe src="data:application/pdf;base64,{pdf_base64}#page={document.metadata["page"]}" width="80%" height="1000px"></iframe>  \n """
     return response
     
 def query_chroma(query):
@@ -49,5 +63,5 @@ def query_chroma(query):
     print(f"Querying for: '{query}'")
     results = chroma_store.similarity_search(query, k=3)
     unique_results = remove_duplicate_results(results)
-    #response = create_response(unique_results)
+    response = create_response(unique_results)
     return unique_results
