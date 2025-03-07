d8cbcf2716787d7d772084ba52a0520969fc7e5e
Elay Binyamin <72104388+eb2514@users.noreply.github.com>
2025-03-06 19:14:25 -0500

    Update rag_v4_cuda.py

--- rag_v4_cuda.py
+++ rag_v4_cuda.py
@@ -10,6 +10,7 @@ import os
 import time
 import chromadb
 import streamlit as st
+from streamlit_pdf_viewer import pdf_viewer
 
 
 
@@ -44,6 +45,7 @@ def create_response(unique_results):
         file_path = document.metadata['source']
         file_name = os.path.basename(file_path).replace('\\', '/')
         file_url = f'{file_endpoint}{file_name}'
+        pdf_viewer("https://806a-2607-fea8-3fb2-3800-71b7-4f42-4d2a-805f.ngrok-free.app/get_file/Pubmed/epj-10-6215.PMC5853996.pdf"")
         #st.write(file_url)
         #get_request = requests.get(file_url, add_headers)
         #if get_request.status_code == 200:
