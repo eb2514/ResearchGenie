6e860f7f78b2cd17133ba3f9f73b37c4d78427e0
Elay Binyamin <72104388+eb2514@users.noreply.github.com>
2025-03-13 21:29:38 -0400

    Update home.py

--- home.py
+++ home.py
@@ -311,7 +311,7 @@ st.markdown(f"""
     .full-width-header {{
         background-color: #8B4513;
         padding: 15px 0;
-        width: 120%;
+        width: 110%;
         position: relative;
         left: 50%;
         right: 50%;
