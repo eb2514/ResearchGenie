8c19aaa494f0bc6d305eea3c69dc7bf65757a8a5
Elay Binyamin <72104388+eb2514@users.noreply.github.com>
2025-03-16 14:05:19 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import streamlit as st
 
-st.set_page_config(page_title="My App",page_icon="🧞‍♂️", layout="wide", initial_sidebar_state="expanded")
-st.markdown(""" 
-            <style>
-            /* Set a fixed width for the sidebar */
-            .stSidebar {
-        width: 400px !important;  /* Set the desired width */
-                        }   
-            <style>    
-            """, unsafe_allow_html=True)
+st.set_page_config(page_title="My App",page_icon="🧞‍♂️", layout="wide")#, initial_sidebar_state="expanded")
+# st.markdown(""" 
+#             <style>
+#             /* Set a fixed width for the sidebar */
+#             .stSidebar {
+#         width: 400px !important;  /* Set the desired width */
+#                         }   
+#             <style>    
+#             """, unsafe_allow_html=True)
 
 pg = st.navigation([
     st.Page("home.py", title="Home",  icon="🧞‍♂️"),
