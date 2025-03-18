590a38ebdcf2305e6895fe2427e7d325dc9662ed
Elay Binyamin <72104388+eb2514@users.noreply.github.com>
2025-03-17 22:37:14 -0400

    Update rag_v4_cuda.py

--- rag_v4_cuda.py
+++ rag_v4_cuda.py
@@ -70,7 +70,7 @@ def query_chroma(query):
     # Query the database
     print(f"Querying for: '{query}'")
     results = chroma_store.similarity_search_with_relevance_scores(query, k=2)
-    st.write(results)
+    #st.write(results)
     relevant_results = [result for result in results if result['score'] >= 0.5]
     if len(relevant_results) > 4:
         unique_results = remove_duplicate_results(relevant_results)
